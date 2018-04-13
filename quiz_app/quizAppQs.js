3045a58f40bd5b16b26cd6f2897c8c3d54de50f7
Carlos G <carlosjr3000@hotmail.com>
2018-04-12 21:12:08 -0500

    added quiz app files

---
+++ quiz_app/quizAppQs.js
@@ -0,0 +1,72 @@
+var questions = [{
+    "question": "What is the capital of Rhode Insland?",
+    "option1": "Hartford",
+    "option2": "Providence",
+    "option3": "Albany",
+    "option4": "Foxboro",
+    "answer": "2"
+}, {
+    "question": "How many continents are there?",
+    "option1": "5",
+    "option2": "2",
+    "option3": "7",
+    "option4": "8",
+    "answer": "3"
+}, {
+    "question": "What is earth's largest continent by surface size?",
+    "option1": "Asia",
+    "option2": "North America",
+    "option3": "Europe",
+    "option4": "Africa",
+    "answer": "1"
+}, {
+    "question": "Which country has the longest coastline in the world?",
+    "option1": "Canada",
+    "option2": "indonesia",
+    "option3": "Australia",
+    "option4": "Russia",
+    "answer": "1"
+}, {
+    "question": "What is Earth's largest ocean by surface?",
+    "option1": "Indian",
+    "option2": "Atlantic",
+    "option3": "Pacific",
+    "option4": "Artic",
+    "answer": "3"
+}];
+//{
+//     "question": "UNIVAC is an example of",
+//     "option1": "First generation computer",
+//     "option2": "Second generation computer",
+//     "option3": "Third generation computer",
+//     "option4": "Fourth generation computer",
+//     "answer": "1"
+// }, {
+//     "question": "Which of the following is an example of non volatile memory ?",
+//     "option1": "VLSI",
+//     "option2": "LSI",
+//     "option3": "ROM",
+//     "option4": "RAM",
+//     "answer": "3"
+// }, {
+//     "question": "Graphic interfaces were first used in a xerox product is called ?",
+//     "option1": "Ethernet",
+//     "option2": "Inter LISP",
+//     "option3": "Small talk",
+//     "option4": "Zeta LISP",
+//     "answer": "1"
+// }, {
+//     "question": "Find the odd one out ?",
+//     "option1": "ORACLE",
+//     "option2": "SYBASE",
+//     "option3": "C",
+//     "option4": "INFORMIX",
+//     "answer": "3"
+// }, {
+//     "question": "The ------ is the administrative section of the computer system?",
+//     "option1": "Memory Unit",
+//     "option2": "Input Unit",
+//     "option3": "Central Processing Unit",
+//     "option4": "Control Unit",
+//     "answer": "3"
+// }
